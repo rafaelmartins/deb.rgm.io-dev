80b9fb4ae81b645398714c42b7048cb880b736c7
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2023-08-20 05:57:39 +0200

    avoid re-evaluating pbuilder-chroots latest tag

--- .scripts/repositories_matrix.sh
+++ .scripts/repositories_matrix.sh
@@ -2,12 +2,14 @@
 
 set -Eeuo pipefail
 
-if [[ $# -ne 2 ]]; then
+if [[ $# -ne 3 ]]; then
     echo "error: Invalid number of arguments" > /dev/stderr
     exit 2
 fi
 
-baseurl="$("${1}/.scripts/pbuilder-chroots_baseurl.sh")"
+srcdir="${1}"
+reposdir="${2}"
+baseurl="${3}"
 
 distros=()
 for distro in $(curl --silent --location "${baseurl}/DISTROS"); do
@@ -15,14 +17,14 @@ for distro in $(curl --silent --location "${baseurl}/DISTROS"); do
 done
 
 repos=()
-for f in $(find "${1}" -type f -name must-rebuild.sh); do
+for f in $(find "${srcdir}" -type f -name must-rebuild.sh); do
     repos+=( "$(basename "$(dirname "${f}")")" )
 done
 
 matrix=()
 for repo in "${repos[@]}"; do
     for distro in "${distros[@]}"; do
-        if "${1}/${repo}/must-rebuild.sh" "${2}" "$(echo "${distro}" | cut -d_ -f2)"; then
+        if "${srcdir}/${repo}/must-rebuild.sh" "${reposdir}" "$(echo "${distro}" | cut -d_ -f2)"; then
             matrix+=( "${repo} ${distro}" )
         fi
     done
