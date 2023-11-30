4e811b7a0f149fc7f5efa92af8460ed13252dfde
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2023-11-29 21:41:15 -0300

    added deb cleanup

--- .scripts/commit-build.sh
+++ .scripts/commit-build.sh
@@ -12,8 +12,50 @@ if [[ ! -d "${NEW_DIR}" ]]; then
     exit 0
 fi
 
+pushd "${DEB_DIR}" > /dev/null
+
+# cleanup removed repositories
+for drepo in *; do
+    found=
+    for mrepo in $("${SCRIPT_DIR}/metadata-repos.sh" "${ROOT_DIR}"); do
+        if [[ "${drepo}" = "${mrepo}" ]]; then
+            found=1
+            break
+        fi
+        if [[ "${drepo}" = "${mrepo}-snapshot" ]]; then
+            found=1
+            break
+        fi
+    done
+    if [[ -z "${found}" ]]; then
+        rm -rvf "${drepo}"
+        continue
+    fi
+
+    pushd "${drepo}" > /dev/null
+
+    for cname in *; do
+        found=
+        for distro in $("${SCRIPT_DIR}/metadata-distros.sh"); do
+            codename="$(echo "${distro}" | cut -d_ -f2)"
+            if [[ "${cname}" = "${codename}" ]]; then
+                found=1
+                break
+            fi
+        done
+        if [[ -z "${found}" ]]; then
+            rm -rvf "${cname}"
+        fi
+    done
+
+    popd > /dev/null
+done
+
+popd > /dev/null
+
 pushd "${NEW_DIR}" > /dev/null
 
+# add new files
 for bdir in build--*; do
     build_id="$(echo "${bdir}" | cut -d- -f3-)"
     repo_name="$(echo "${build_id}" | cut -d' ' -f1)"
