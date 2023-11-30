6c3ac97901fe5b2040e5a8c761f24f1eb6cef045
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2023-11-29 21:16:33 -0300

    added orig cleanup

--- .scripts/commit-orig.sh
+++ .scripts/commit-orig.sh
@@ -12,8 +12,31 @@ if [[ ! -d "${NEW_DIR}" ]]; then
     exit 0
 fi
 
+pushd "${ORIG_DIR}" > /dev/null
+
+# cleanup removed repositories
+for orepo in *; do
+    found=
+    for mrepo in $("${SCRIPT_DIR}/metadata-repos.sh" "${ROOT_DIR}"); do
+        if [[ "${orepo}" = "${mrepo}" ]]; then
+            found=1
+            break
+        fi
+        if [[ "${orepo}" = "${mrepo}-snapshot" ]]; then
+            found=1
+            break
+        fi
+    done
+    if [[ -z "${found}" ]]; then
+        rm -rvf "${orepo}"
+    fi
+done
+
+popd > /dev/null
+
 pushd "${NEW_DIR}" > /dev/null
 
+# add new files
 for odir in orig--*; do
     dir="$(echo "${odir}" | cut -d- -f3-)"
     orig="$(basename "$(ls -1 "${odir}"/*.orig.* | head -n1)")"
