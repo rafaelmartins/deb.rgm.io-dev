340c23194a4643e4bbd5bf509bf20c6143d26256
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2023-11-29 23:32:15 -0300

    Revert "Revert "Revert "remove"""
    This reverts commit e84199c1aa52a73efbd745a1dd9eaf43c43ba98f.

---
+++ filebin/orig.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+NUM_ARGS=1
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)/../.scripts"
+source "${SCRIPT_DIR}/utils.sh"
+
+OUTPUT_DIR="$(realpath "${1}")"
+
+version="$(
+    git describe \
+        --abbrev=4 \
+        HEAD \
+    | sed \
+        -e 's/^v//' \
+        -e 's/-/./' \
+        -e 's/-g/-/'
+)"
+
+git archive \
+    --format=tar \
+    --prefix="filebin-${version}/" \
+    HEAD \
+| xz \
+> "${OUTPUT_DIR}/filebin_${version}.orig.tar.xz"
+
+echo "${version}" > "${OUTPUT_DIR}/VERSION"
